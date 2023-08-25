9d40973ee73b685adc24ab0ebde622895f602487
Rongjian Zhang <pd4d10@gmail.com>
2023-08-26 02:30:38 +0800

    feat: add options

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,44 @@
 import { IndexHtmlTransformResult, Plugin } from "vite";
 
-export default function vitePluginDevtools(): Plugin {
-  let serve = false;
+export interface VitePluginDevtoolsOptions {
+  /**
+   * https://github.com/Tencent/vConsole
+   */
+  vconsole?: boolean;
+  /**
+   * https://github.com/liriliri/eruda
+   */
+  eruda?: boolean;
+}
+
+export default function vitePluginDevtools({
+  vconsole,
+  eruda,
+}: VitePluginDevtoolsOptions): Plugin {
+  let build = true;
 
   return {
     name: "vite-plugin-devtools",
     configResolved({ command }) {
-      serve = command === "serve";
+      build = command === "build";
     },
     transformIndexHtml() {
-      if (serve) {
-        return [
-          { tag: "script", attrs: { src: "https://unpkg.com/vconsole" } },
-          { tag: "script", children: "new VConsole()" },
-          { tag: "script", attrs: { src: "https://unpkg.com/eruda" } },
-          { tag: "script", children: "eruda.init()" },
-        ] satisfies IndexHtmlTransformResult;
-      }
+      if (build) return;
+
+      return [
+        ...(vconsole
+          ? [
+              { tag: "script", attrs: { src: "https://unpkg.com/vconsole" } },
+              { tag: "script", children: "new VConsole()" },
+            ]
+          : []),
+        ...(eruda
+          ? [
+              { tag: "script", attrs: { src: "https://unpkg.com/eruda" } },
+              { tag: "script", children: "eruda.init()" },
+            ]
+          : []),
+      ] satisfies IndexHtmlTransformResult;
     },
   };
 }
