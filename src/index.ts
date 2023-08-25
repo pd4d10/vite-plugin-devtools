f19f5388f6f883f68a67ca569f06c8a7e2da5183
Rongjian Zhang <pd4d10@gmail.com>
2023-08-26 02:26:21 +0800

    feat: add vconsole and eruda

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import { Plugin } from "vite";
+import { IndexHtmlTransformResult, Plugin } from "vite";
 
 export default function vitePluginDevtools(): Plugin {
+  let serve = false;
+
   return {
     name: "vite-plugin-devtools",
-    // TODO:
+    configResolved({ command }) {
+      serve = command === "serve";
+    },
+    transformIndexHtml() {
+      if (serve) {
+        return [
+          { tag: "script", attrs: { src: "https://unpkg.com/vconsole" } },
+          { tag: "script", children: "new VConsole()" },
+          { tag: "script", attrs: { src: "https://unpkg.com/eruda" } },
+          { tag: "script", children: "eruda.init()" },
+        ] satisfies IndexHtmlTransformResult;
+      }
+    },
   };
 }
